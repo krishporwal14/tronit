160f7eee11387be893263402e9f3a2c4c440dd7f
Krish Porwal <101163531+krishporwal14@users.noreply.github.com>
2026-03-16 20:18:23 +0530

    refactor: added better error handling in hash_object.rs.

--- src/commands/hash_object.rs
+++ src/commands/hash_object.rs
@@ -1,11 +1,13 @@
+use anyhow::{Context, Result};
 use std::fs;
 
 use crate::object;
 
-pub fn run(file: &str) {
-    let data = fs::read(file).unwrap();
+pub fn run(file: &str) -> Result<()> {
+    let data = fs::read(file).with_context(|| format!("failed to read file: {}", file))?;
 
-    let hash = object::write_object("blob", &data);
+    let hash = object::write_object("blob", &data)?;
 
     println!("{}", hash);
+    Ok(())
 }
